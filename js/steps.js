ebcd0c4a4b4c6d5225a6059d4064b2cb3d3f36cd
Yulia Petrushenko <ypetrushenko@fix.ru>
2019-06-26 17:56:06 +0300

    Add step animation

--- js/steps.js
+++ js/steps.js
@@ -3,8 +3,30 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 const tl = new TimelineLite()
+const svg = '#order-svg'
 
 function initAnimation () {
-  TweenLite.set('#order-svg path, #order-svg line, #order-svg circle', {visibility: 'visible'})
-  tl.from('#order-svg path, #order-svg line, #order-svg circle', 2, {drawSVG: 0, ease: Power3.easeIn})
+  TweenLite.set('path, line, circle', {visibility: 'visible'})
+
+  tl.from('.window *', 1, {drawSVG: 0, ease: Power3.easeIn}, 0)
+    .from('.list1 *, .list2 *', 1, {drawSVG: 0, ease: Power3.easeIn}, 0)
+
+  const listLines = document.querySelectorAll('.lines *')
+
+  listLines.forEach((x, i) => {
+    tl.from(x, 0.1, {drawSVG: 0, ease: Power3.easeIn})
+  })
+
+  const circles = document.querySelectorAll('.circles *')
+  let circlesStart = tl.endTime()
+
+  circles.forEach((x, i) => {
+    tl.from(x, 0.2, {drawSVG: 0, ease: Power3.easeIn})
+  })
+
+  const dashes = document.querySelectorAll('.dashes *')
+
+  dashes.forEach((x, i) => {
+    tl.from(x, 0.1, {drawSVG: 0, ease: Power3.easeIn}, circlesStart + 0.1 * i)
+  })
 }
